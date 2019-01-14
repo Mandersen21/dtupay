2817dfa06819bbc622c0bf9a0c8c35d283628577
mandersen21 <mikkelandersen21@gmail.com>
2019-01-14 13:29:59 +0100

    Added customer-service

---
+++ customer-service/src/main/java/beijing/customerservice/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package beijing.customerservice.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
