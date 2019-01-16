1abd903c947acd607064900f170c4d49f35a8cff
Cristian Botezatu <s164571@student.dtu.dk>
2019-01-16 14:16:36 +0100

    changes

--- customer-service/src/main/java/beijing/customerservice/rest/CustomerEndpoint.java
+++ customer-service/src/main/java/beijing/customerservice/rest/CustomerEndpoint.java
@@ -33,7 +33,7 @@ public class CustomerEndpoint {
 
 	public CustomerEndpoint() {
 		customerManager = new CustomerManager(customerRepository);
-		listToken.add("123");
+		listToken.add("12345");
 		customerRepository.createCustomer(new Customer("123", "123102", "Stephen", listToken));
 	}
 	
