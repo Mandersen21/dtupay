4f3f258006904f008a90aacfbcb7ee895a8295dd
mandersen21 <mikkelandersen21@gmail.com>
2019-01-17 10:57:40 +0100

    Added new dependency

--- token-service/src/main/java/beijing/tokenservice/domain/TokenManager.java
+++ token-service/src/main/java/beijing/tokenservice/domain/TokenManager.java
@@ -51,9 +51,12 @@ public class TokenManager {
 		factory = new ConnectionFactory();
 		factory.setUsername("admin");
 		factory.setPassword("Banana");
-
+		//factory.setUsername("guest");
+		//factory.setUsername("guest");
+		
 		factory.setHost("02267-bejing.compute.dtu.dk");
-
+		//factory.setHost("localhost");
+		
 		connection = factory.newConnection();
 		channel = connection.createChannel();
 		channel.queueDeclare(TOKENID_TO_MERCHANTSERVICE_QUEUE, false, false, false, null);
