d8aebad3e8b6ab2c43067048b528654b3106c078
Cristian Botezatu <s164571@student.dtu.dk>
2019-01-18 12:19:05 +0100

    Changes customer

--- customer-service/src/main/java/beijing/customerservice/repository/CustomerRepository.java
+++ customer-service/src/main/java/beijing/customerservice/repository/CustomerRepository.java
@@ -1,36 +1,17 @@
 package beijing.customerservice.repository;
 
 import java.util.ArrayList;
-
 import java.util.List;
-import java.util.Random;
-
 import beijing.customerservice.domain.Customer;
 
 public class CustomerRepository implements ICustomerRepository {
     private List<Customer> customerList = new ArrayList<>();
 
-    private static CustomerRepository ourInstance = new CustomerRepository();
-
-    public static CustomerRepository getInstance() {
-        return ourInstance;
-    }
-
-    public String generateRandomCustomerId(int length) {
-		int m = (int) Math.pow(10, length - 1);
-		return Integer.toString(m + new Random().nextInt(9 * m));
-	}
-    
-    public String addCustomer(String name, String cpr, List<String> tokenList) {
-    	String id = generateRandomCustomerId(3);
-        Customer customer = new Customer(id, name, cpr, tokenList);
-  
-        customerList.add(customer);
-        return id;
-    }
-
-    public List<Customer> getCustomerList() {
-        return customerList;
+    public boolean createCustomer(Customer customer) {
+    	if (customerList.contains(customer) == false) {
+    		return customerList.add(customer);
+    	}
+		return false;
     }
 
     public Customer getCustomerById(String id) {
@@ -43,11 +24,11 @@ public class CustomerRepository implements ICustomerRepository {
         return null;
     }
     
-    public List<Customer> removeCustomer(Customer customer) {
+    public boolean removeCustomer(Customer customer) {
         if (getCustomerById(customer.getId()) != null) {
-        	customerList.remove(customer);
+        	return customerList.remove(customer);
         } 
-        return customerList; 
+        return false; 
     }
 
 	public boolean customerExists(Customer customer) {
@@ -63,4 +44,5 @@ public class CustomerRepository implements ICustomerRepository {
         }
         return null;
     }
+
 }
--- customer-service/src/main/java/beijing/customerservice/rest/CustomerRestService.java
+++ customer-service/src/main/java/beijing/customerservice/rest/CustomerRestService.java
@@ -1,34 +1,39 @@
 package beijing.customerservice.rest;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import beijing.customerservice.domain.Customer;
+import beijing.customerservice.domain.CustomerManager;
 import beijing.customerservice.exception.CustomerNotFoundException;
-import beijing.customerservice.repository.CustomerRepository;
-
+import beijing.customerservice.exception.RequestRejected;
+import java.io.IOException;
 import java.util.List;
 
 @Path("customers")
 public class CustomerRestService {
-    private CustomerRepository dataService = CustomerRepository.getInstance();
-
-    //localhost:8080/customers (Output all the customers)
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Customer> getCustomers() {
-        return dataService.getCustomerList();
-    }
+    private CustomerManager customerManager;
 
     //localhost:8080/customers (Need to place a json file in the Body)
     @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public String createCustomer(
+    public Response createCustomer(@FormParam("id") String id,
     						     @FormParam("name") String name,
                                  @FormParam("cpr") String cpr,
                                  @FormParam("tokenList") List<String> tokenList) {
-        return dataService.addCustomer(name, cpr, tokenList);
+        try {
+        	if (customerManager.addCustomer(id, cpr, name, tokenList)) {
+        		return Response.ok().entity(customerManager.getCustomerById(id)).build();
+        	}
+        	return Response.status(500).build();
+		} catch (RequestRejected e) {
+			return Response.status(404).build();
+		} catch (IOException e) {
+			return Response.status(500).build();
+		} catch (CustomerNotFoundException e) {
+			return Response.status(404).build();
+		}
     }
 
     //localhost:8080/customers/1 (if it exists, otherwise - create it first)
@@ -36,9 +41,15 @@ public class CustomerRestService {
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getCustomer(@PathParam("id") String id) {
-        Customer customer = dataService.getCustomerById(id);
+        Customer customer;
+		try {
+			customer = customerManager.getCustomerById(id);
+		} catch (CustomerNotFoundException e) {
+			return Response.status(404).build();
+		}
+		
         if (customer == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
+            return Response.status(404).build();
         } else {
             return Response.ok().entity(customer).build();
         }
@@ -49,11 +60,21 @@ public class CustomerRestService {
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response deleteCustomer(@PathParam("id") String id) {
-    	Customer customer = dataService.getCustomerById(id);
+    	Customer customer;
+		try {
+			customer = customerManager.getCustomerById(id);
+		} catch (CustomerNotFoundException e) {
+			return Response.status(404).build();
+		}
+		
     	if (customer == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
+            return Response.status(404).build();
         } else {
-            return Response.ok().entity(dataService.removeCustomer(customer)).build();
+            try {
+				return Response.ok().entity(customerManager.removeCustomer(customer)).build();
+			} catch (CustomerNotFoundException e) {
+				return Response.status(404).build();
+			}
         }
     }
 }
