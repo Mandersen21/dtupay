2d123f471c755ec583b44942e9914ac07f1fe268
Nicolaj Pedersen <nicolajstaldpedersen@gmail.com>
2019-01-22 11:52:26 +0100

    cu stuff

--- system-tests/src/main/java/beijing/CustomerSimulator.java
+++ system-tests/src/main/java/beijing/CustomerSimulator.java
@@ -42,12 +42,6 @@ public class CustomerSimulator {
 	
 
 
-	public DTUPayCustomerResponse getToken(String custId) throws UnirestException {
 
-		HttpResponse<String> result = Unirest.post(dtupayUrl + ":3000/tokens").header("Content-Type", "application/x-wwww-form-urlencoded")
-				.header("Accept", "application/json").queryString("name", custId).field("amount", 1).asString();
-		return new DTUPayCustomerResponse(result.getStatus(), result.getBody());
-		
-	}
 
 }
