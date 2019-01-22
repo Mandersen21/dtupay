6b9494167018e452d6e80e8844293f1f9186a2c7
Cristian Botezatu <s164571@student.dtu.dk>
2019-01-22 21:40:01 +0100

    changes

--- customer-service/src/test/java/CustomerManagerTest.java
+++ customer-service/src/test/java/CustomerManagerTest.java
@@ -1,14 +1,11 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
-
 import org.junit.Before;
 import org.junit.Test;
-
 import beijing.customerservice.domain.Customer;
 import beijing.customerservice.domain.CustomerManager;
 import beijing.customerservice.exception.ConnectionException;
