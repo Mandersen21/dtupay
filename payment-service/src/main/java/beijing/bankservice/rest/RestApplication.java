99ffb2118b20ecfe13191f4d85d0fb0868f23dbe
mandersen21 <mikkelandersen21@gmail.com>
2019-01-22 15:09:13 +0100

    Change endpoint

--- payment-service/src/main/java/beijing/bankservice/rest/RestApplication.java
+++ payment-service/src/main/java/beijing/bankservice/rest/RestApplication.java
@@ -1,12 +1,26 @@
 package beijing.bankservice.rest;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 import beijing.bankservice.domain.BankServiceManager;
+import beijing.bankservice.repository.IPaymentRepository;
 import beijing.bankservice.repository.PaymentRepository;
 
 @ApplicationPath("/")
 public class RestApplication extends Application {
 
+	private static IPaymentRepository repository = new PaymentRepository();
+	private BankServiceManager manager;
+	
+	public RestApplication() {
+		try {
+			manager = new BankServiceManager(repository);
+		} catch (IOException | TimeoutException e) {
+			e.printStackTrace();
+		}
+	}
 }
